8b86a912a684a96d20b34d08c9436c4a01c8ca57
Jeffrey Tamatini TEAHUI <45190210+tamatini@users.noreply.github.com>
2023-08-31 20:53:41 +0200

    shopping cart page styling

--- src/Controller/ShoppingCartController.php
+++ src/Controller/ShoppingCartController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\ShoppingCart;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/shopping_cart', name: 'shopping_cart_')]
 class ShoppingCartController extends AbstractController
 {
-    public function index() : Response
+    #[Route('/', name: 'index')]
+    public function index(EntityManagerInterface $entityManager) : Response
     {
         /** @var User $user */
         $user = $this->getUser();
         if ($user) {
-            $shoppingCart = $user->getShoppingCart();
+            $shoppingCart = $entityManager->getRepository(ShoppingCart::class)->findOneBy(['user'=>$user]);
         } else {
             $shoppingCart = new ShoppingCart();
         }
